60446443f1134ba2bfbead4327ba67fdcfbd1a6f
Manuel Sanson <manu.sanson9@gmail.com>
2023-03-10 16:54:09 -0300

    add github and linkedin links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,6 +8,15 @@ const Footer = () => {
 
     return (
         <footer className='footerContainer'>
+            <div className='socialMedia'>
+                <a href="https://github.com/ManuelSanson" target="_blank" rel="noreferrer">
+                    <img src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt='GitHub Logo'/>
+                </a>
+                <a href="https://www.linkedin.com/in/manuel-sanson-trans-dev/?locale=en_US" target="_blank" rel="noreferrer">
+                    <img src='https://cdn-icons-png.flaticon.com/512/49/49408.png' alt='LinkedIn Logo'/>
+                </a>
+
+            </div>
             <div className='footerLogo'>
                 <img src='https://res.cloudinary.com/diqry3ihg/image/upload/v1669564167/DevPortfolio/MSFrontendLogo_kuoxx6.png' alt='Manuel Sanson Logo' onClick={()=> scrollToSection(home)} className='projectsLink'/>
             </div>
